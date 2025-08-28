07bb1a86c3f6d0d549268f69f7fc225b719cede0
Akobir <akobirortiqov40@gmail.com>
2025-08-28 16:09:47 +0500

    finish generic views

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import UserProfilAPIview, TaskAPIView
+from .views import UserProfilAPIview, TaskAPIView,UserProfileDetailView, TaskListCreateView, TaskDetailView, TaskByStatusView, OverdueTasksView
 
 
 urlpatterns = [
     path('api/profile/', UserProfilAPIview.as_view(), name='profile'),
     path('api/tasks/', TaskAPIView.as_view(), name='task-list-create'),
     path('api/tasks/<int:pk>/', TaskAPIView.as_view(), name='task-detail'),    
-    path('<int:pk>/', UserProfilAPIview.as_view())
+    path('<int:pk>/', UserProfilAPIview.as_view()),
+    path('profile/', UserProfileDetailView.as_view(), name='profile-detail'),
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/status/<str:status>/', TaskByStatusView.as_view(), name='tasks-by-status'),
+    path('tasks/overdue/', OverdueTasksView.as_view(), name='overdue-tasks'),
 ]
--- user/views.py
+++ user/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from .models import UserProfile, Task
 from .serializers import UserSerializer, TaskSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
+from django.utils import timezone
 
 class UserProfilAPIview(APIView):
     def get(self, request, pk = None):
@@ -71,18 +72,18 @@ class TaskAPIView(APIView):
     def get(self, request, pk = None):
         if pk:
             try:
-                user = Task.objects.get(pk=pk)
+                task = Task.objects.get(pk=pk)
             except Task.DoesNotExist:
 
                 return Response(status=status.HTTP_404_NOT_FOUND)
             
-            serializer = TaskSerializer(user)
+            serializer = TaskSerializer(task)
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         else:
-            users = Task.objects.all()
-            serializer = TaskSerializer(users, many=True)
+            tasks = Task.objects.filter(status="todo", priority="high")
+            serializer = TaskSerializer(tasks, many=True)
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         
@@ -116,3 +117,60 @@ class TaskAPIView(APIView):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+
+
+class UserProfileDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return UserProfile.objects.get(user=self.request.user)
+
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+    
+
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+    
+
+
+class TaskByStatusView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        status = self.kwargs.get("status")  
+        return Task.objects.filter(user=self.request.user, status=status)
+    
+
+
+class OverdueTasksView(generics.ListAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        today = timezone.now().date()
+        return Task.objects.filter(
+            user=self.request.user,
+            due_date__lt=today
+        ).exclude(status='done')
+
+
+
